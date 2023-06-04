222575955d4ea6233415e7412a6ad2f41af8da8f
Sophie Bremer <44302338+sophiebremer@users.noreply.github.com>
2023-06-04 08:37:23 +0200

    Pass on HTTP header from the CGI script (#10)
    Fix #2; HTTP header of CGI send in HTTP body.

---
+++ tst/CGIHandler.test.ts
@@ -0,0 +1,45 @@
+/*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*\
+
+  SVR: Simple HTTP(S) Server
+
+  Copyright (c) TypeScriptLibs and Contributors
+
+  Licensed under the MIT License; you may not use this file except in
+  compliance with the License. You may obtain a copy of the MIT License at
+  https://typescriptlibs.org/LICENSE.txt
+
+\*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*/
+
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+
+import test from '@typescriptlibs/tst';
+
+
+/* *
+ *
+ *  Tests
+ *
+ * */
+
+
+test( 'Test CGIHandler body', async ( assert: test.Assert ) => {
+    const response = await fetch( 'http://localhost:9876/cgi-bin/test.sh' );
+
+    assert.strictEqual(
+        response.headers.get( 'Content-Type' ),
+        'image/svg+xml; charset=utf-8',
+        'Header of CGI response should contain SVG content type and UTF-8.'
+    );
+
+    assert.strictEqual(
+        await response.text(),
+        '<svg xmlns="http://www.w3.org/2000/svg"><text y="16">🤓</text></svg>\n',
+        'Body of CGI response should contain SVG image with UTF-8 emoji.'
+    );
+} );
