90db6d406738fd51a93e5a0d9c5a93ac55df09d2
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2023-06-17 23:29:37 +0200

    Add test for bugfix #14

---
+++ tst/FileHandler.test.ts
@@ -0,0 +1,45 @@
+/*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*\
+
+  SVR: Simple HTTP(S) Server
+
+  Copyright (c) TypeScriptLibs and Contributors
+
+  Licensed under the MIT License; you may not use this file except in
+  compliance with the License. You may obtain a copy of the MIT License at
+  https://typescriptlibs.org/LICENSE.txt
+
+\*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*/
+
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+
+import test from '@typescriptlibs/tst';
+
+
+/* *
+ *
+ *  Tests
+ *
+ * */
+
+
+test( 'Test FileHandler dot-files', async ( assert: test.Assert ) => {
+    const response = await fetch( 'https://localhost:9876/.hidden' );
+
+    assert.strictEqual(
+        response.headers.get( 'Content-Type' ),
+        'text/html; charset=utf-8',
+        'Header of CGI response should contain HTML content type and UTF-8.'
+    );
+
+    assert.strictEqual(
+        response.status,
+        404,
+        'HTTP status should not reveal file existence.'
+    );
+} );
