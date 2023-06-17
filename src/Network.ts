254ef73b45371ebddb69c076bb0de790bde191cf
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2023-06-17 17:30:44 +0200

    Add port search and shift defaults to 8000 range

---
+++ src/Network.ts
@@ -0,0 +1,96 @@
+/*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*\
+
+  SVR: Simple HTTP(S) Server
+
+  Copyright (c) TypeScriptLibs and Contributors
+
+  Licensed under the MIT License; you may not use this file except in
+  compliance with the License. You may obtain a copy of the MIT License at
+  https://typescriptlibs.org/LICENSE.txt
+
+\*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*/
+
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+
+import Net from 'node:net';
+
+
+/* *
+ *
+ *  Declarations
+ *
+ * */
+
+
+export interface NetworkError extends Error {
+    address: string;
+    code: string;
+    errno: number;
+    port: number;
+    syscall: string;
+}
+
+
+/* *
+ *
+ *  Functions
+ *
+ * */
+
+
+function connect (
+    port: number,
+    host?: string
+): Promise<Net.Socket> {
+    return new Promise( ( resolve, reject ) => {
+        try {
+            Net.connect( port, host )
+                .once( 'error', reject )
+                .once( 'ready', resolve )
+        }
+        catch ( e ) {
+            reject( e );
+        }
+    } );
+}
+
+
+async function freePort (
+    firstPort = 1000,
+    lastPort = 65536
+): Promise<number> {
+
+    for ( let port = firstPort; port < lastPort; ++port ) {
+        try {
+            ( await connect( port ) ).end();
+        }
+        catch ( e ) {
+            if ( ( e as NetworkError ).code === 'ECONNREFUSED' ) {
+                return port;
+            }
+        }
+    }
+
+    throw new Error( 'No socket found' );
+}
+
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+
+export const Network = {
+    connect,
+    freePort,
+};
+
+export default Network;
